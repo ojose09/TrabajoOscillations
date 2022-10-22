91e1d3c3658a706b37d267701e56f18d32bd731a
Jose ortiz <109627243+ojose09@users.noreply.github.com>
2022-10-22 00:21:19 -0500

    Oscillations

--- Trabajo_Oscillations/Assets/Scripts/Oscillations.cs
+++ Trabajo_Oscillations/Assets/Scripts/Oscillations.cs
@@ -18,9 +18,9 @@ public class Oscillations : MonoBehaviour
     void Update()
     {
         float x = amplitude * Mathf.Sin(2f * Mathf.PI * (Time.time / period));
-        transform.position = initialPosition + new Vector3(x, 0, 0);
+        transform.position = initialPosition + new Vector3(x, x, 0);
 
         float y = Mathf.Sin(5f * Time.time) + Mathf.Cos(Time.time / 3f) + Mathf.Sin(Time.time / 13f);
-        transform.position = initialPosition + new Vector3(x, x, 0);
+        transform.position = initialPosition + new Vector3(x, 0, 0);
     }
 }
